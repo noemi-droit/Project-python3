23e9f9f950967d2d593716125fa5b441389dd228
noemi-droit llampa <noemillampa50@gmail.com>
2024-03-13 13:08:23 +0000

    Save new changes in python

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+board = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-",]
+
+currentPlayer = "X"
+winner= None
+gameRunning= True
+
+# game board
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("---------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("---------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print("---------")
+
+# take player input
+def playerInput(board):
+    inp = int(input("Select a spot 1-9: "))
+    if board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("Oops player is already at that spot.")
+
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+
