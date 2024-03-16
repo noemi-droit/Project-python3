53058fe43a9d7b8c540c6dbdac6a7d065ba2bc93
noemi-droit llampa <noemillampa50@gmail.com>
2024-03-16 01:30:00 +0000

    save changes to run.py

--- run.py
+++ run.py
@@ -15,37 +15,19 @@ def printBoard(board):
     print("---------")
     print(board[6] + " | " + board[7] + " | " + board[8])
     print("---------")
-
-# take player input
-def playerInput(board):
-    while True: 
-        inp =input("Select a spot 1-9: ")
-        if int(inp) in range(1,9):
-            break
-        else:
-            print("that input will be invalid")
-            continue
-    if board[inp-1] == "-":
-        board[inp-1] = currentPlayer
-    else:
-        print("Oops player is already at that spot.")
-
- # check for win or tie
-def checkHorizontal(board):
-     global winner
-     if board[0] == board[1] == board[2] and board[0] != "-":
-        winner = board[0]
-        return True
-     elif board[3] == board[4] == board[5] and board[3] != "-":
-        winner = board[3]
-        return True
-     elif board[6] == board[7] == board[8] and board[6] != "-":
-        winner = board[6]
-        return True
- # switch the player
- # check for win or tie again 
-
-while gameRunning:
-    printBoard(board)
-    playerInput(board)
-
+# check if a player has won
+def check_win(board, player):
+    win-combinations==[
+        [board[0],board[1],board[2]],
+        [board[3], board[4], board[5]],
+        [board[6], board[7], board[8]],
+        [board[0], board[3], board[6]],
+        [board[1], board[4], board[7]],
+        [board[2], board[5], board[8]],
+        [board[0], board[4], board[8]],
+        [board[2], board[4], board[6]],
+]
+for combination in win_combinations:
+    if all(cell==player for cell in combination):
+       return True
+    return False
