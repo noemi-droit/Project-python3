9fce5e9057a56f830ead886628e2052570b5b171
noemi-droit llampa <noemillampa50@gmail.com>
2024-03-21 12:38:24 +0000

    save new cahnges

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ When you create the app, you will need to add two buildpacks from the _Settings_
 
 You must then create a _Config Var_ called `PORT`. Set this to `8000`
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+If you have credentials, such as in the Love Sandwiches project, you must create another _`heroku/python`` and paste the JSON into the value field.
 
 Connect your GitHub repository and deploy as normal.
 
--- run.py
+++ run.py
@@ -19,8 +19,8 @@ def printBoard(board):
 
 # check if a player has won
 def check_win(board, player):
-    win-combinations==[
-        [board[0],board[1],board[2]],
+    win_combinations = [
+        [board[0], board[1], board[2]],
         [board[3], board[4], board[5]],
         [board[6], board[7], board[8]],
         [board[0], board[3], board[6]],
@@ -30,7 +30,7 @@ def check_win(board, player):
         [board[2], board[4], board[6]],
 ]
 for combination in win_combinations:
-    if all(cell==player for cell in combination):
+    if all( cell == player for cell in combination):
        return True
        return False
 
@@ -41,16 +41,17 @@ def play_game():
     while True:
         print_board(board)
         move=int(input(f"Player{player}, enter your move (1-9):"))-1
-        if board [move] ==player
-           if check_win(board,player):
-            print_board(board)
-            print(f"Player{player}wins!")
-            break
-            if ''not in board:
+        if board[move]=='':
+            board[move]= player
+        if check_win(board,player):
+              print_board(board)
+              print(f"Player{player}wins!")
+              break
+        if ''not in board:
                 print_board(board)
                 print("It's a tie!")
                 break
-            player="O" if player = "X" else "X"
-            else:
-                print("Invalid move, try again")
+                player="O" if player =="X" else "X"
+        else:
+                print("Invalid move, try again.")
 
