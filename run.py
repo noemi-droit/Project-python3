9509ad9b04b6ef3d012cca9b2d8fdfe76bac24cf
noemi-droit llampa <noemillampa50@gmail.com>
2024-04-16 23:18:35 +0000

    Save new changes to pyhton structure

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import random
+
 board = ["-", "-", "-",
         "-", "-", "-",
         "-", "-", "-",]
@@ -7,7 +7,7 @@ currentPlayer = "X"
 winner= None
 gameRunning= True
 
-# game board
+# Printing the game board
 def printBoard(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
     print("---------")
@@ -16,42 +16,7 @@ def printBoard(board):
     print(board[6] + " | " + board[7] + " | " + board[8])
     print("---------")
 
-
-# check if a player has won
-def check_win(board, player):
-    win_combinations = [
-        [board[0], board[1], board[2]],
-        [board[3], board[4], board[5]],
-        [board[6], board[7], board[8]],
-        [board[0], board[3], board[6]],
-        [board[1], board[4], board[7]],
-        [board[2], board[5], board[8]],
-        [board[0], board[4], board[8]],
-        [board[2], board[4], board[6]],
-]
-for combination in win_combinations:
-    if all( cell == player for cell in combination):
-       return True
-       return False
-
-# play game
-def play_game():
-    board = [""]*9
-    player = "X"
-    while True:
-        print_board(board)
-        move=int(input(f"Player{player}, enter your move (1-9):"))-1
-        if board[move]=='':
-            board[move]= player
-        if check_win(board,player):
-              print_board(board)
-              print(f"Player{player}wins!")
-              break
-        if ''not in board:
-                print_board(board)
-                print("It's a tie!")
-                break
-                player="O" if player =="X" else "X"
-        else:
-                print("Invalid move, try again.")
-
+#Take player input
+#Check your win or tie
+#Switch the player
+#Check for win or tie again
